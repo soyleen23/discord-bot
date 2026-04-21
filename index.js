9138e3d47a904518d5826efb923d6fd3356a346a
soyleen23 <leonardo23danna@gmail.com>
2026-04-21 04:02:17 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bot activo');
+});
+
+app.listen(3000, () => {
+  console.log('Web funcionando');
+});
+
 const { 
   Client, 
   GatewayIntentBits, 
