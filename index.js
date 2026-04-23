392bd744b8160fdb544b0f7725df1a42774ae926
soyleen23 <leonardo23danna@gmail.com>
2026-04-22 20:33:17 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -36,7 +36,8 @@ const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers //Empleados
   ]
 });
 
